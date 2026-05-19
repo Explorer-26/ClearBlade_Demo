857837792fb89a42fac9003812b902538eb9db59
Explorer-26 <amrita.cys.dt@gmail.com>
2026-05-19 14:23:57 +0530

    Update line-chart-meter.js

--- line-chart-meter.js
+++ line-chart-meter.js
@@ -1,1 +1,38 @@
 
+parser = (ctx) => {
+
+  if (!ctx.datasource || ctx.datasource.length === 0) {
+    return [];
+  }
+
+  var raw = ctx.datasource;
+
+  raw.sort((a, b) => {
+
+    return new Date(a.data.timestamp) -
+           new Date(b.data.timestamp);
+
+  });
+
+  var points = [];
+
+  for (var i = 0; i < raw.length; i++) {
+
+    var ts = raw[i].data.timestamp;
+
+    ts = ts.split(".")[0];
+
+    points.push({
+
+      x: new Date(ts).toLocaleTimeString(),
+
+      y: Number(
+        raw[i].data.current_load_kw
+      ) || 0
+
+    });
+
+  }
+
+  return points;
+}
