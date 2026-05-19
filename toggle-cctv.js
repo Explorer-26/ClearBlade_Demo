66f3e7a7c055ef9c0be0d7e56449f5c4265b6ce2
Explorer-26 <amrita.cys.dt@gmail.com>
2026-05-19 14:20:01 +0530

    Create toggle-cctv.js

---
+++ toggle-cctv.js
@@ -0,0 +1,20 @@
+parser = (ctx) => {
+
+  if (!ctx.datasource || ctx.datasource.length === 0) {
+    return false;
+  }
+
+  var raw = ctx.datasource;
+
+  raw.sort((a, b) => {
+
+    return new Date(a.data.timestamp) -
+           new Date(b.data.timestamp);
+
+  });
+
+  var latest = raw[raw.length - 1];
+
+  return latest.data.motion_detected === true;
+
+}
